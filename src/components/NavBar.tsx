34c83c679844d2264ebf28a16304bae07608a60c
Letam Davidson <davidsonletam@gmail.com>
2022-12-13 12:14:01 +0100

    Check the file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,14 @@ function NavBar() {
 	return (
 		<div className="bg-base flex flex-between">
 			<h2 className="text-white px-3 py-2">DODO4U</h2>
-			{[
+			{/* {[
 				["Home", "/dashboard"],
 				["Team", "/team"],
 			].map(([title, url]) => (
 				<a href={url} className="px-3 py-2 text-white">
 					{title}
 				</a>
-			))}
+			))} */}
 		</div>
 	);
 }
